dbc69491c9e6fa61927248caa98e898ee58c70e4
herrylo <herryloyopai@163.com>
2020-05-22 13:42:55 +0800

    del logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './style/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+      
     </div>
   );
 }
